da9592a0214057a14078e36d2128f75a4b59271c
Rodion <67863695+Weadmead@users.noreply.github.com>
2020-10-24 14:49:13 +0300

    added global-wrapper + glowing effects + some fixes

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ $(function () {
     //Липкий хедер
     window.addEventListener('scroll', function () {
         var header = document.querySelector('header');
-        header.classList.toggle('sticky', window.scrollY > 0);
+        header.classList.toggle('sticky', window.scrollY > 100);
     });   
         
    // Маркер под кнопками меню навигации
