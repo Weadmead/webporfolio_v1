15cdabe7df97936dc1aa3aedadc1e54ec783d13a
Rodion <67863695+Weadmead@users.noreply.github.com>
2020-11-19 14:42:36 +0200

    added burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ $(function () {
     //Липкий хедер
     window.addEventListener('scroll', function () {
         var header = document.querySelector('header');
-        header.classList.toggle('sticky', window.scrollY > 400);
+        header.classList.toggle('sticky', window.scrollY > 200);
     });   
         
    // Маркер под кнопками меню навигации
@@ -19,26 +19,39 @@ $(function () {
             indicator(e.target);
         })      
     })
-// добавляем цвет активной кнопке выбора языка
+    // добавляем цвет активной кнопке выбора языка
     $('.translate').on('click', function() {
     $(this).addClass('active')
         .siblings().removeClass('active');
-})
+    })
+    // кнопка меню 
+    $('.header-menu__btn').on('click', function () {
+        $('.header-menu__btn').toggleClass('header-menu__btn--active'); 
+        $('.header-menu__list').toggleClass('header-menu__list--active'); 
+    });
+
+    $('.header-menu__list-item').on('click', function () {
+        $('.header-menu__btn').toggleClass('header-menu__btn--active'); 
+        $('.header-menu__list').toggleClass('header-menu__list--active'); 
+    });
 
       
-$("nav").on("click","a", function (event) {
+    $("nav").on("click","a", function (event) {
         // исключаем стандартную реакцию браузера
-        event.preventDefault();
- 
+        event.preventDefault();    
         // получем идентификатор блока из атрибута href
-        var id  = $(this).attr('href'),
- 
+        var id  = $(this).attr('href'),    
         // находим высоту, на которой расположен блок
-            top = $(id).offset().top;
-         
+            top = $(id).offset().top;            
         // анимируем переход к блоку, время: 800 мс
         $('body,html').animate({scrollTop: top}, 800);
-});
+    });
+    $(".scroll__btn").on("click","a", function (event) {       
+        event.preventDefault();       
+        var id  = $(this).attr('href'),       
+            top = $(id).offset().top;       
+        $('body,html').animate({scrollTop: top}, 800);
+    });
     
     //custom scrollbar
     $(window).scroll(function () {
@@ -50,46 +63,46 @@ $("nav").on("click","a", function (event) {
     })
     // gallery
     const addListeners = div => {
-    div.addEventListener('mouseenter', () => {
-        div.classList.add('hover')
-    })
-    div.addEventListener('mouseleave', () => {
-        div.classList.remove('hover')
-    })
-}
+        div.addEventListener('mouseenter', () => {
+            div.classList.add('hover')
+        })
+        div.addEventListener('mouseleave', () => {
+            div.classList.remove('hover')
+        })
+    }
 
     const container = document.querySelector('.grid-container')
-    addListeners(container)
+        addListeners(container)
 
     const allGalleryContainer = document.querySelectorAll('.works__gallery')
-    allGalleryContainer.forEach(galleryContainer => {
-    addListeners(galleryContainer)
+        allGalleryContainer.forEach(galleryContainer => {
+        addListeners(galleryContainer)
     })
     
     // slider
-  $('.testemonials__slider').slick({    
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    dots: true,
-    arrows: false
-  });
+    $('.testemonials__slider').slick({    
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false
+    });
   
     // form
-    	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
+    $("form").submit(function() { //Change
+	    var th = $(this);
+	    $.ajax({
+		    type: "POST",
+		    url: "mail.php", //Change
+		    data: th.serialize()
+	    }).done(function() {
+		    alert("Thank you!");
+		    setTimeout(function() {
+			// Done Functions
+			th.trigger("reset");
+		}, 1000);
+	});
+	return false;
 	});
 
 });
